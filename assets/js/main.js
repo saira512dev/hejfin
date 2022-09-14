1de418e75caea1d4aea9e65e1507c0f8a8728bf3
saira512dev <512saira@gmail.com>
2022-09-14 19:55:55 +0530

    Made the navbar sticky

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,16 @@
 window.onscroll = function() {fixNavbar()};
 
 const navbar = document.getElementById("navbar");
+const alert = document.querySelector(".alert");
 
 var sticky = navbar.offsetTop;
 
 function fixNavbar() {
   if (window.pageYOffset > sticky) {
     navbar.classList.add("sticky")
+    alert.classList.add("move");
   } else {
     navbar.classList.remove("sticky");
+    alert.classList.remove("move");
   }
 }
