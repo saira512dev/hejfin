b495c91761f74ff1c67c3c83ed7386455293a969
saira512dev <512saira@gmail.com>
2022-09-27 17:54:54 +0530

    Menu is now responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,21 @@ window.onscroll = function() {fixNavbar()};
 
 const navbar = document.getElementById("navbar");
 const alert = document.querySelector(".alert");
-const header = document.querySelector
+const mobileMenuIcon = document.querySelector('.mobile-menu-button')
+const mobileMenu = document.querySelector('.mobile-menu')
+const mobileLinks = document.querySelectorAll('.mobile-link')
 
-var sticky = navbar.offsetTop;
-console.log(window.screen.availWidth)
+let sticky = navbar.offsetTop;
+
+mobileMenuIcon.addEventListener("click", displayMobileMenu)
+
+mobileLinks.forEach(link => link.addEventListener("click", displayMobileMenu))
+
+function displayMobileMenu() {
+  mobileMenu.classList.toggle('hidden')
+}
+
+
 
 function fixNavbar() {
   if (window.pageYOffset > sticky && window.screen.availWidth > 768) {
