651be60eb6b11e1e8eb50ed9e64ae7114a839fb8
saira512dev <512saira@gmail.com>
2022-09-27 12:47:22 +0530

    Tried fixing heaer and alert, started with mobile menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,24 @@ window.onscroll = function() {fixNavbar()};
 
 const navbar = document.getElementById("navbar");
 const alert = document.querySelector(".alert");
+const header = document.querySelector
 
 var sticky = navbar.offsetTop;
+console.log(window.screen.availWidth)
 
 function fixNavbar() {
-  if (window.pageYOffset > sticky) {
+  if (window.pageYOffset > sticky && window.screen.availWidth > 768) {
     navbar.classList.add("sticky")
     alert.classList.add("move");
   } else {
     navbar.classList.remove("sticky");
     alert.classList.remove("move");
   }
-}
+
+//   if (window.pageYOffset < sticky && window.screen.availWidth < 420) {
+//     console.log("HERE");
+//     alert.classList.add('move-mobile');
+//   } else {
+//     alert.classList.remove('move-mobile')
+//   }
+ }
