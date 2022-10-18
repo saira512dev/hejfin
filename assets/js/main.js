cd09fc29413904a619cce177ef9efbc918c4f42c
saira512dev <512saira@gmail.com>
2022-10-18 14:14:33 +0530

    Replaced contact form with calendly

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,10 @@ const alert = document.querySelector(".alert");
 const mobileMenuIcon = document.querySelector('.mobile-menu-button')
 const mobileMenu = document.querySelector('.mobile-menu')
 const mobileLinks = document.querySelectorAll('.mobile-link')
+const calendlyButton = document.querySelector('.calendly-popup')
+const calendly = document.querySelector('.calendly')
+const calendlyClose = document.querySelector('.calendly-close')
+const calendlyFrame = document.querySelector('.calendly-frame')
 
 let sticky = navbar.offsetTop;
 let stickyMobile = mobileMenu.offsetTop;
@@ -10,6 +14,25 @@ let stickyMobile = mobileMenu.offsetTop;
 mobileMenuIcon.addEventListener("click", displayMobileMenu)
 
 mobileLinks.forEach(link => link.addEventListener("click", displayMobileMenu))
+
+calendlyButton.addEventListener("click", showCalendly)
+
+calendlyClose.addEventListener("click", hideCalendly)
+
+function hideCalendly(){
+  calendly.classList.add("hidden")
+  calendlyButton.classList.remove("hidden")
+  calendlyFrame.classList.add('hidden')
+  calendlyClose.classList.add('hidden')
+}
+
+function showCalendly() {
+  console.log("YEEEY")
+  calendly.classList.remove("hidden")
+  calendlyButton.classList.add("hidden")
+  calendlyFrame.classList.remove('hidden')
+  calendlyClose.classList.remove('hidden')
+}
 
 function displayMobileMenu() {
   mobileMenu.classList.toggle('hidden')
